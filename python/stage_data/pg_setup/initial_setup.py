332b91afaa89d1956afbfa21b2914a14f6f77a75
Dan Finkel <danfinkel@dans-mbp.lan>
2022-06-20 20:03:21 -0400

    db setup

--- python/stage_data/pg_setup/initial_setup.py
+++ python/stage_data/pg_setup/initial_setup.py
@@ -1,61 +1,98 @@
 import psycopg2
 
-DEFAULT_CXN = {'db': 'postgres',
-               'user': 'danfinkel',
-               'password': 'password',
-               'host': '127.0.0.1',
-               'port': '5432'
-              }
+class db_setup():
+    def __init__(self):
 
-CRIMEUSER_CXN = {'db': 'crime_project',
-                 'user': 'crimeuser',
-                 'password': 'password',
-                 'host': '127.0.0.1',
-                 'port': '5432'
-              }
+        self.DEFAULT_CXN = {'db': 'postgres',
+                            'user': 'danfinkel',
+                            'password': 'password',
+                            'host': '127.0.0.1',
+                            'port': '5432'
+                           }
+
+        self.CRIMEUSER_CXN = {'db': 'crime_project',
+                         'user': 'crimeuser',
+                         'password': 'password',
+                         'host': '127.0.0.1',
+                         'port': '5432'
+                      }
 
 
-def submit_sql(cxn, sql):
-    """
-    one off sql execution via psycopg2
-    """
+    def submit_sql(cxn, sql):
+        """
+        one off sql execution via psycopg2
+        """
 
-    conn = psycopg2.connect(
-                 database= cxn['db'],
-                 user= cxn['user'],
-                 password= cxn['password'],
-                 host= cxn['host'],
-                 port= cxn['port']
-            )
-    conn.autocommit = True
+        conn = psycopg2.connect(
+                     database= cxn['db'],
+                     user= cxn['user'],
+                     password= cxn['password'],
+                     host= cxn['host'],
+                     port= cxn['port']
+                )
+        conn.autocommit = True
 
-    #Creating a cursor object using the cursor() method
-    cursor = conn.cursor()
+        #Creating a cursor object using the cursor() method
+        cursor = conn.cursor()
 
-    try:
-        cursor.execute(sql)
-    except:
-        print('sql did not execute')
+        try:
+            cursor.execute(sql)
+            print('sql executed')
+        except Exception as e:
+            print('sql did not execute')
+            print(e)
+            conn.close()
+
         conn.close()
 
-    print('sql executed')
-    conn.close()
+
+    def create_crimeuser():
+        """
+        run this first
+        """
+
+        sql = '''CREATE role {crimeuser} LOGIN password '{password}' '''.format(crimeuser=CRIMEUSER_CXN['user'],
+                                                                                password=CRIMEUSER_CXN['password']
+                                                                               )
+        submit_sql(DEFAULT_CXN,sql)
 
 
-def create_crimeuser():
-    """
-    run this first
-    """
+    def create_crimedb():
+        """
+        run this second
+        """
 
-    sql = '''CREATE role {crimeuser} LOGIN password '{password}' '''.format(crimeuser=CRIMEUSER_CXN['user'],
-                                                                            password=CRIMEUSER_CXN['password']
-                                                                           )
-    submit_sql(DEFAULT_CXN,sql)
+        sql = '''CREATE database {crime_project}'''.format(crime_project=CRIMEUSER_CXN['db'])
+        submit_sql(DEFAULT_CXN,sql)
+
+        sql = '''GRANT ALL PRIVILEGES ON DATABASE {crime_project} TO {user};'''.format(crime_project=CRIMEUSER_CXN['db'],
+                                                                                       user=CRIMEUSER_CXN['user'])
+        submit_sql(DEFAULT_CXN,sql)
 
 
-def create_crimedb():
-    """
-    """
+    def create_schema(schema_name):
+        """
+        """
 
-    sql = '''CREATE database {crime_project}'''.format(crime_project=CRIMEUSER_CXN['db'])
-    submit_sql(DEFAULT_CXN,sql)
+        sql = '''CREATE schema {schema_name}'''.format(schema_name=schema_name)
+        submit_sql(CRIMEUSER_CXN, sql)
+
+
+    def drop_db():
+        """
+        Be careful
+        """
+
+        sql = '''DROP database {crime_project} WITH (FORCE)'''.format(crime_project=CRIMEUSER_CXN['db'])
+        submit_sql(DEFAULT_CXN,sql)
+
+    def run():
+
+        # first drop the db
+        self.drop_db()
+
+        # create the project db
+        create_crimedb()
+
+        # create the nibrs schema
+        create_schema(schema_name='nibrs')
